2eca9d52c1fa2575ac15a8af8320cffaab93b432
RendersC <crewsrenders@gmail.com>
2025-05-11 18:06:33 +0500

    VERTEX

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,41 @@
-public class Vertex {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Vertex)) return false;
+        Vertex<?> other = (Vertex<?>) obj;
+        return data.equals(other.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
