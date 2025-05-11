412fe220788eb56ae7eabaa976d04523bf2cb4e9
RendersC <crewsrenders@gmail.com>
2025-05-11 18:07:22 +0500

    UG

--- src/UnweightedGraph.java
+++ src/UnweightedGraph.java
@@ -1,2 +1,21 @@
-public class UnweightedGraph {
+import java.util.*;
+
+public class UnweightedGraph<V> {
+    private final boolean directed;
+    private final Map<V, List<V>> adjacencyList = new HashMap<>();
+
+    public UnweightedGraph(boolean directed) {
+        this.directed = directed;
+    }
+
+    public void addEdge(V source, V destination) {
+        adjacencyList.computeIfAbsent(source, k -> new ArrayList<>()).add(destination);
+        if (!directed) {
+            adjacencyList.computeIfAbsent(destination, k -> new ArrayList<>()).add(source);
+        }
+    }
+
+    public Map<V, List<V>> getAdjacencyList() {
+        return adjacencyList;
+    }
 }
